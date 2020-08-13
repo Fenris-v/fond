02af4a5eee79dfb5abd58033bd42187a216da6aa
Fenris <anatolijv236@gmail.com>
2020-08-13 18:57:25 +0300

    add adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,23 @@ $(document).ready(() => {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/news/arrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/news/arrow.png"></button>',
         slidesToShow: 1,
-        autoplay: true
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', function (e) {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
     });
 });
